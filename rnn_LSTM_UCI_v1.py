b01c972871c9e8157a9ffb815ffd19edd1393132
KennCoder7 <iskenn7@gmail.com>
2018-11-07 17:02:27 +0800

    Newest version

--- rnn_LSTM_UCI_v1.py
+++ rnn_LSTM_UCI_v1.py
@@ -3,62 +3,76 @@ import tensorflow as tf
 from collections import Counter
 
 train_x = np.load('data/processed/np_train_x.npy')
-train_y = np.load('data/processed/np_train_y.npy')
-# train_x_one_hot = np.load("data/processed/np_train_y_one_hot.npy")
+train_y = np.load("data/processed/np_train_y_one_hot.npy")
 test_x = np.load('data/processed/np_test_x.npy')
-test_y = np.load('data/processed/np_test_y.npy')
-# test_x_one_hot = np.load("data/processed/np_test_x_one_hot.npy")
-print("### train_x shape:", train_x.shape, "###")
-print("### test_x shape:", test_x.shape, "###")
-print("### train_y shape:", train_y.shape, Counter(train_y), "###")
-print("### test_y shape:", test_y.shape, Counter(test_y), "###")
-print("### Process --- data load --- finished ###")
+test_y = np.load("data/processed/np_test_y_one_hot.npy")
+print("# train_x shape:", train_x.shape, "###")
+print("# test_x shape:", test_x.shape, "###")
+print("# train_y shape:", train_y.shape, "###")
+print("# test_y shape:", test_y.shape, "###")
+print("# Process --- data load --- finished ###")
 
 train_data_count = train_x.shape[0]
 test_data_count = test_x.shape[0]
 n_steps = train_x.shape[1]
 n_input = train_x.shape[2]
 
-n_hidden = 32  # Hidden layer num of features
-n_classes = 6  # Total classes (should go up, or should go down)
+n_hidden = 32
+n_classes = 6
 
 learning_rate = 0.0025
-lambda_loss_amount = 0.0015
-training_steps = 1000  # Loop 1000 times
-batch_size = 1500
-display_iter = 30000  # To show test set accuracy during training
+training_steps = 10000  # Loop 10000 times
+batch_size = 1000
 
 
-def LSTM_RNN(_X, _weights, _biases):
-    _X = tf.transpose(_X, [1, 0, 2])  # (128, ?, 9)
-    print("# transpose shape: ", _X.shape)    # transpose shape:  (128, ?, 9)
-    _X = tf.reshape(_X, [-1, n_input])  # (n_step*batch_size, n_input)
-    print("# reshape shape: ", _X.shape)    # reshape shape:  (?, 9)
-    _X = tf.nn.relu(tf.matmul(_X, _weights['hidden']) + _biases['hidden'])
-    print("# matmul shape: ", _X.shape)     # matmul shape:  (?, 32)
-    _X = tf.split(_X, n_steps, 0)  # n_steps * (batch_size, n_hidden)
-    print("# split shape: ", np.array(_X).shape)    # split shape:  (128,)
+def lstm_rnn(_x):
+    _x = tf.transpose(_x, [1, 0, 2])  # (128, ?, 9)
+    print("# transpose shape: ", _x.shape)    # transpose shape:  (128, ?, 9)
+    _x = tf.reshape(_x, [-1, n_input])  # (n_step*batch_size, n_input)
+    print("# reshape shape: ", _x.shape)    # reshape shape:  (?, 9)
+    _x = tf.layers.dense(
+        inputs=_x,
+        units=n_hidden,
+        activation=tf.nn.relu,
+    )
+    print("# relu shape: ", _x.shape)     # relu shape:  (?, 32)
+    _x = tf.split(_x, n_steps, 0)  # n_steps * (batch_size, n_hidden)
+    # spilt makes _x.type from array --> list for static_rnn()
+    print("# list shape: ", np.array(_x).shape)    # list shape:  (128,)
+    print("# list unit shape: ", np.array(_x)[0].shape)    # list unit shape:  (?, 32)
 
-    lstm_cell_1 = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_1 = tf.nn.rnn_cell.LSTMCell(num_units=n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_1_drop = tf.nn.rnn_cell.DropoutWrapper(cell=lstm_cell_1, output_keep_prob=0.5)
     print("# cell_1 shape: ", lstm_cell_1.state_size)   # cell_1 shape:  LSTMStateTuple(c=32, h=32)
-    lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=True)
+
+    lstm_cell_2 = tf.nn.rnn_cell.LSTMCell(num_units=n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_2_drop = tf.nn.rnn_cell.DropoutWrapper(cell=lstm_cell_2, output_keep_prob=0.5)
     print("# cell_2 shape: ", lstm_cell_2.state_size)   # cell_2 shape:  LSTMStateTuple(c=32, h=32)
-    lstm_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
+
+    lstm_cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell_1_drop, lstm_cell_2_drop], state_is_tuple=True)
     print("# multi cells shape: ", lstm_cells.state_size)
     # multi cells shape:  (LSTMStateTuple(c=32, h=32), LSTMStateTuple(c=32, h=32))
 
-    outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _X, dtype=tf.float32)
+    outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _x, dtype=tf.float32)
     print("# outputs & states shape: ", np.array(outputs).shape, np.array(states).shape)
     # outputs & states shape:  (128,) (2, 2)
 
     lstm_last_output = outputs[-1]  # N to 1
     print("# last output shape: ", lstm_last_output.shape)  # last output shape:  (?, 32)
+
+    lstm_last_output = tf.layers.dense(
+        inputs=lstm_last_output,
+        units=n_hidden,
+        activation=tf.nn.relu
+    )
+    print("# fully connected shape: ", lstm_last_output.shape)  # fully connected shape:  (?, 32)
+
     softmax = tf.layers.dense(
         inputs=lstm_last_output,
         units=n_classes,
         activation=tf.nn.softmax
     )
-    # return tf.matmul(lstm_last_output, _weights['out']) + _biases['out']
+    print("# softmax shape: ", softmax.shape)  # softmax shape:  (?, 6)
     return softmax
 
 
@@ -77,59 +91,36 @@ def extract_batch_size(_train, step, batch_size):
     return batch_s
 
 
-def one_hot(y_, n_classes=n_classes):
-    # Function to encode neural one-hot output labels from number indexes
-    # e.g.:
-    # one_hot(y_=[[5], [0], [3]], n_classes=6):
-    #     return [[0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0]]
-
-    y_ = y_.reshape(len(y_))
-    return np.eye(n_classes)[np.array(y_, dtype=np.int32)]  # Returns FLOATS
-
-
 x = tf.placeholder(tf.float32, [None, n_steps, n_input], name='x')
 y = tf.placeholder(tf.float32, [None, n_classes], name='y')
 
-weights = {
-    'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])),  # Hidden layer weights
-    'out': tf.Variable(tf.random_normal([n_hidden, n_classes], mean=1.0))
-}
-biases = {
-    'hidden': tf.Variable(tf.random_normal([n_hidden])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
-}
+pred = lstm_rnn(x)
 
-pred = LSTM_RNN(x, weights, biases)
-
-# Loss, optimizer and evaluation
-l2 = lambda_loss_amount * sum(
-    tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables()
-)  # L2 loss prevents this overkill neural network to overfit the data
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred)) + l2  # Softmax loss
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)  # Adam Optimizer
+loss = - tf.reduce_mean(y * tf.log(tf.clip_by_value(pred, 1e-10, 1.0)))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)  # Adam Optimizer
 
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
-    # step = 1
     for step in range(training_steps):
         batch_xs = extract_batch_size(train_x, step, batch_size)
-        batch_ys = one_hot(extract_batch_size(train_y, step, batch_size))
-
+        batch_ys = extract_batch_size(train_y, step, batch_size)
         # Fit training using batch data
-        _, loss, acc = sess.run([optimizer, cost, accuracy],
-                                feed_dict={x: batch_xs, y: batch_ys})
-        if (step+1) % 10 == 0:
-            print("Step", (step+1), "| Train loss:", loss,
-                  "| Train accuracy:", acc)
+        _, loss_train, acc_train = sess.run([optimizer, loss, accuracy],
+                                            feed_dict={x: batch_xs, y: batch_ys})
         if (step+1) % 50 == 0:
-            loss, acc = sess.run([cost, accuracy],
-                                 feed_dict={x: test_x, y: one_hot(test_y)})
-            print("Step", (step+1), "| Test loss:", loss,
-                  "| Test accuracy:", acc)
+            print("# Step", (step+1), "| Train loss:", loss_train,
+                  "| Train accuracy:", acc_train)
+        if (step+1) % 200 == 0:
+            loss_test, acc_test = sess.run([loss, accuracy],
+                                           feed_dict={x: test_x, y: test_y})
+            print("# Step", (step+1), "| Test loss:", loss_test,
+                  "| Test accuracy:", acc_test)
 
-# 2018/11/7
-# Step 1000 | Train loss: 1.191127 | Train accuracy: 0.98733336
-# Step 1000 | Test loss: 1.320142 | Test accuracy: 0.8574822
+# 2018/11/7 with fc
+# Step 5600 | Train loss: 0.012740456 | Train accuracy: 0.984
+# Step 5600 | Test loss: 0.06892706 | Test accuracy: 0.9165253
+# Step 5800 | Train loss: 0.01251974 | Train accuracy: 0.98
+# Step 5800 | Test loss: 0.052563805 | Test accuracy: 0.92365116
